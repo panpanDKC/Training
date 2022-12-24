72f1b7ef8d576f997db128242c7d14220f730b95
panpanDKC <97619055+panpanDKC@users.noreply.github.com>
2022-12-24 01:44:35 +0100

    add transpose, copy, dumb mult, pow and sub to matrix

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -10,6 +10,17 @@ matrix* init_mat(size_t rows, size_t cols){
 	return res;
 }
 
+matrix* copy_mat(matrix* m){
+	matrix* res = init_mat(m->rows, m->cols);
+
+	for(size_t i = 0; i < m->rows; i++){
+		for(size_t j = 0; j < m->cols; j++){
+			res->data[i * m->cols +j] = m->data[i * m->cols + j];
+		}
+	}
+	return res;
+}
+
 void print_mat(matrix* m){
 	for(size_t i = 0; i < m->rows; i++){
 		printf("||");
@@ -35,6 +46,37 @@ matrix* add_mat(matrix* m1, matrix* m2){
 	return res;
 }
 
+matrix* sub_mat(matrix* m1, matrix* m2){
+	if(m1->cols != m2->cols || m1->rows != m2->rows){
+		err(1, "Addition : dimensions do not fit !\n");
+	}
+	
+	matrix* res = init_mat(m1->rows, m1->cols);
+
+	for(size_t i = 0; i < m1->rows; i++){
+		for(size_t j = 0; j < m1->cols; j++){
+			res->data[i * m1->cols + j] = m1->data[i * m1->cols + j] - m2->data[i * m1->cols + j];
+		}
+	}
+	return res;
+}
+
+matrix* dumb_mult_mat(matrix* m1, matrix* m2){
+	if(m1->cols != m2->cols || m1->rows != m2->rows){
+		err(1, "Addition : dimensions do not fit !\n");
+	}
+
+	matrix* res = init_mat(m1->rows, m2->cols);
+
+	for(size_t i = 0; i < m1->rows; i++){
+		for(size_t j = 0; j < m1->cols; j++){
+			res->data[i * m1->cols + j] = m1->data[i * m1->cols +j]
+				* m2->data[i * m1->cols + j];
+		}
+	}
+	return res;
+}
+
 matrix* dot_mat(matrix* m1, matrix* m2){
 	if(m1->cols != m2->rows){
 		err(1,"Multiplication : dimensions do not fit !\n");
@@ -46,6 +88,36 @@ matrix* dot_mat(matrix* m1, matrix* m2){
 			for(size_t k = 0; k < m1->cols; k++){
 				res->data[i*res->cols+j] += m1->data[i*m1->cols+k] * m2->data[k*m2->cols+j];
 			}
+		}
+	}
+	return res;
+}
+
+matrix* pow_mat(matrix* m, int x){
+	matrix* res = copy_mat(m);
+	while(x > 1){
+		res = dumb_mult_mat(res,res);
+		x--;
+	}
+	return res;
+}
+
+matrix* transpose_mat(matrix* m){
+	matrix* res = init_mat(m->cols, m->rows);
+
+	for(size_t i = 0; i < res->rows; i++){
+		for(size_t j = 0; j < res->cols; j++){
+			res->data[i * res->cols + j] = m->data[j*m->cols + i];
+		}
+	}
+	return res;
+}
+
+double sum_of_mat(matrix* m){
+	double res = 0;
+	for(size_t i = 0; i < m->rows; i++){
+		for(size_t j = 0; j < m->cols; j++){
+			res += m->data[i * m->cols + j];
 		}
 	}
 	return res;
@@ -69,5 +141,18 @@ int main(){
 	printf("m3 : \n");
 	matrix* m3 = dot_mat(m1,m2);
 	print_mat(m3);
+
+	printf("transpose m1 :\n");
+	matrix* t_m1 = transpose_mat(m1);
+	print_mat(t_m1);
+
+
+	printf("sum of m2 :\n");
+	double sum_m2 = sum_of_mat(m2);
+	printf("%.2f\n",sum_m2);
+
+	printf("pow m2 of 3 :\n");
+	matrix* pow_m2 = pow_mat(m2,2);
+	print_mat(pow_m2);
 	return 1;
 }
--- matrix/matrix.h
+++ matrix/matrix.h
@@ -8,6 +8,12 @@ typedef struct matrix{
 }matrix;
 
 matrix* init_mat(size_t rows, size_t cols);
+matrix* copy_mat(matrix* m);
 void print_mat(matrix* m);
 matrix* add_mat(matrix* m1, matrix* m2);
+matrix* sub_mat(matrix* m1, matrix* m2);
 matrix* dot_mat(matrix* m1, matrix* m2);
+matrix* pow_mat(matrix* m, int x);
+matrix* dumb_mult_mat(matrix* m1, matrix* m2);
+matrix* transpose_mat(matrix* m);
+double sum_of_mat(matrix* m);
